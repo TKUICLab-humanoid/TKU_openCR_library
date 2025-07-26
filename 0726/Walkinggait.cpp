3f7bc300bf057fa8740d0638194c21bff07499d6
nugusheiyou <iclabhumanoid@gmail.com>
2025-07-26 19:34:42 +0800

    for control only leg

---
+++ 0726/Walkinggait.cpp
@@ -0,0 +1,427 @@
+#include "Walkinggait.h"
+#include <math.h>
+#include <Arduino.h>
+WalkingGait::WalkingGait(){
+}
+void WalkingGait::initial(){
+  zmp_x = 0;
+  zmp_y = 0;
+  width_x = 0;
+  width_y = 0;
+  hand = 0;
+  var_theta = 0;
+  footstep_x = 0;
+  footstep_y = -width_size;
+  displacement_x = 0;
+  displacement_y = 0;
+  base_x = 0;
+  base_y = 0;
+  sample_point = 0;
+  now_step = 0;
+  pre_step = -1;
+  walking_state = 0;  // 步態狀態
+  step_ = 99999;
+  ready_to_stop = false;
+  continuous_flag = true;
+}
+
+void WalkingGait::continuous(){
+  float TT_ = period_t_ * 0.001;
+  time_point = sample_time * sample_point;
+  t_ = ((float)(time_point % period_t_) + (float)sample_time) / 1000; //步週期內時刻(s)
+  now_step = (sample_point)/(period_t_/ sample_time); 
+  var_theta = theta_ / 180 * M_PI;
+  if (now_step < 2){
+    walking_state = 2; //start
+  }
+  else if (now_step == 2){
+    walking_state = 1; // first
+  }
+  else if (now_step == step_){
+    walking_state = 0; // stop
+  }
+  else if (now_step > step_) {
+    continuous_flag = false;
+    continuous_flag = false;
+    continuous_flag = false;
+    return;
+  }
+  else {
+    walking_state = 3; //continuous
+  }
+
+  if (pre_step != now_step){
+    if (pre_step == -1){
+      footstep_x = 0;
+      footstep_y = -width_size;
+      now_right_x = footstep_x;
+      now_right_y = -width_size;
+      now_left_x = 0;
+      now_left_y = width_size;
+    }
+    else if (pre_step % 2 == 1){
+      now_right_x = footstep_x;
+      now_right_y = footstep_y;
+    }
+    else if (pre_step % 2 == 0){
+      now_left_x = footstep_x;
+      now_left_y = footstep_y;
+    }
+  
+  zmp_x = footstep_x;
+  zmp_y = footstep_y;
+  last_displacement_x = displacement_x;   //上次的跨幅
+  last_base_x = base_x;                   //上次到達的位置
+  last_displacement_y = displacement_y;   //上次的Y軸位移量
+  last_base_y = base_y;                   //上次的Y軸位移位置
+  last_theta = var_theta;                 //前一次的Theta變化量
+
+  if (walking_state == 2) {//start
+    var_theta = 0;
+    now_width = 2 * width_size * (-pow(-1, now_step + 1));
+    width_x = -sin(var_theta) * now_width;
+    width_y = cos(var_theta) * now_width;
+    displacement_x = width_x;
+    displacement_y = width_y;
+    footstep_x = footstep_x + width_x;
+    footstep_y = footstep_y + width_y;   
+  }
+  else if (walking_state == 0) {// stop
+    now_width = 2 * width_size * (-pow(-1, now_step + 1));
+    width_x = -sin(var_theta) * now_width;
+    width_y = cos(var_theta) * now_width;
+    displacement_x = width_x;
+    displacement_y = width_y;
+    footstep_x = footstep_x + width_x;
+    footstep_y = footstep_y + width_y;
+  }
+  else {
+    now_width = 2 * width_size * (-pow(-1, now_step + 1));
+    width_x = -sin(var_theta) * now_width;
+    width_y = cos(var_theta) * now_width;
+    displacement_x = step_length * cos(var_theta) - shift_length * sin(var_theta) + width_x;
+    displacement_y = step_length * sin(var_theta) + shift_length * cos(var_theta) + width_y;
+    footstep_x = footstep_x + displacement_x;
+    footstep_y = footstep_y + displacement_y;
+  }
+  base_x = (footstep_x + zmp_x) / 2;
+  base_y = (footstep_y + zmp_y) / 2;
+  }
+
+  hand = 10 * (1 + abs(0.1*step_length));
+  if (hand > 20) {
+    hand = 20;
+  }
+  if (step_length < 0){
+    hand = -hand;
+  }
+  pre_step = now_step;
+  switch (walking_state) {
+    case 0://stop
+      Cvx = Com_vel(last_base_x, base_x, zmp_x, TT_, Tc_);
+      Cpx = Com_pos(last_base_x, Cvx, zmp_x, t_, Tc_);
+      Cvy = Com_vel(last_base_y, base_y, zmp_y, TT_, Tc_);
+      Cpy = Com_pos(last_base_y, Cvy, zmp_y, t_, Tc_);
+      Cpz = COM_HEIGHT;
+      if (now_step % 2 == 0) {
+        Lx = Swingfoot_pos_XY(now_left_x, (last_displacement_x + displacement_x) / 2, t_, TT_, Tdsp);
+        Ly = Swingfoot_pos_XY(now_left_y, (last_displacement_y + displacement_y) / 2, t_, TT_, Tdsp);
+        Lz = Swingfoot_pos_z(lift_height, t_, TT_, Tdsp);
+
+        Rx = zmp_x;
+        Ry = zmp_y;
+        Rz = 0;
+
+        Lt = 0;
+        Rt = wFootTheta(-last_theta, 1, t_, TT_, Tc_);
+      }
+      else if (now_step % 2 == 1) {
+        Lx = zmp_x;
+        Ly = zmp_y;
+        Lz = 0;
+
+        Rx = Swingfoot_pos_XY(now_right_x, (last_displacement_x + displacement_x) / 2, t_, TT_, Tdsp);
+        Ry = Swingfoot_pos_XY(now_right_y, (last_displacement_y + displacement_y) / 2, t_, TT_, Tdsp);
+        Rz = Swingfoot_pos_z(lift_height, t_, TT_, Tdsp);
+
+        Lt =  wFootTheta(-last_theta, 1, t_, TT_, Tc_);
+        Rt = 0;
+      }
+      break;
+    case 1://first
+      Cvx = Com_vel(0, base_x, zmp_x, TT_, Tc_);
+      Cpx = Com_pos(0, Cvx, zmp_x, t_, Tc_);
+      Cvy = Com_vel(0, base_y, zmp_y, TT_, Tc_);
+      Cpy = Com_pos(0, Cvy, zmp_y, t_, Tc_)+ com_y_swing * sin(M_PI * t_ / TT_);
+      Cpz = COM_HEIGHT;
+      
+      Lx = Swingfoot_pos_XY(now_left_x, displacement_x / 2, t_, TT_, Tdsp);
+      Ly = Swingfoot_pos_XY(now_left_y, (displacement_y - now_width) / 2, t_, TT_, Tdsp);
+      Lz = Swingfoot_pos_z(lift_height, t_, TT_, Tdsp);
+
+      Rx = zmp_x;
+      Ry = zmp_y;
+      Rz = 0;
+          
+      Lt = wFootTheta(-last_theta, 1, t_, TT_, Tc_);
+      Rt = wFootTheta(-var_theta, 0, t_, TT_, Tc_);
+      break;
+    case 2://start
+      Cvx = Com_vel(0, 0, zmp_x, TT_, Tc_);
+      Cpx = Com_pos(0, Cvx, zmp_x, t_, Tc_);
+      Cvy = Com_vel(0, 0, zmp_y, TT_, Tc_); 
+      Cpz = COM_HEIGHT;
+      if (now_step % 2 == 0) {
+        Cpy = Com_pos(0, Cvy, zmp_y, t_, Tc_) + com_y_swing * sin(M_PI * t_ / TT_);
+
+        Lx = Swingfoot_pos_XY(now_left_x, 0, t_, TT_, Tdsp);
+        Ly = Swingfoot_pos_XY(now_left_y, 0, t_, TT_, Tdsp);
+        Lz = Swingfoot_pos_z(lift_height, t_, TT_, Tdsp);
+
+        Rx = zmp_x;
+        Ry = zmp_y;
+        Rz = 0;
+
+        Lt = 0;
+        Rt = 0;
+      }
+      else if (now_step % 2 == 1) {
+        Cpy = Com_pos(0, Cvy, zmp_y, t_, Tc_) - com_y_swing * sin(M_PI * t_ / TT_);
+
+        Lx = zmp_x;
+        Ly = zmp_y;
+        Lz = 0;
+
+        Rx = Swingfoot_pos_XY(now_right_x, 0, t_, TT_, Tdsp);
+        Ry = Swingfoot_pos_XY(now_right_y, 0, t_, TT_, Tdsp);
+        Rz = Swingfoot_pos_z(lift_height, t_, TT_, Tdsp);
+
+        Lt = 0;
+        Rt = 0;
+      }
+      break;
+    case 3://contiuous
+      Cvx = Com_vel(last_base_x, base_x, zmp_x, TT_, Tc_);
+      Cpx = Com_pos(last_base_x, Cvx, zmp_x, t_, Tc_);
+      Cvy = Com_vel(last_base_y, base_y, zmp_y, TT_, Tc_);
+      Cpz = COM_HEIGHT;
+      if (now_step % 2 == 0) {
+        Cpy = Com_pos(last_base_y, Cvy, zmp_y, t_, Tc_)+ com_y_swing * sin(M_PI * t_ / TT_); 
+        Lx = Swingfoot_pos_XY(now_left_x, (last_displacement_x + displacement_x) / 2, t_, TT_, Tdsp);
+        Ly = Swingfoot_pos_XY(now_left_y, (last_displacement_y + displacement_y) / 2, t_, TT_, Tdsp);
+        Lz = Swingfoot_pos_z(lift_height, t_, TT_, Tdsp);
+
+        Rx = zmp_x;
+        Ry = zmp_y;
+        Rz = 0;
+
+        if (var_theta * last_theta >= 0) {
+          Lt = wFootTheta(-var_theta, 0, t_, TT_, Tc_);
+          Rt = wFootTheta(-last_theta, 1, t_, TT_, Tc_);
+        }
+        else {
+          Lt = 0;
+          Rt = 0;
+        }
+      }
+      else if (now_step % 2 == 1) {
+        Cpy = Com_pos(last_base_y, Cvy, zmp_y, t_, Tc_)- com_y_swing * sin(M_PI * t_ / TT_); 
+        Lx = zmp_x;
+        Ly = zmp_y;
+        Lz = 0;
+
+        Rx = Swingfoot_pos_XY(now_right_x, (last_displacement_x + displacement_x) / 2, t_, TT_, Tdsp);
+        Ry = Swingfoot_pos_XY(now_right_y, (last_displacement_y + displacement_y) / 2, t_, TT_, Tdsp);
+        Rz = Swingfoot_pos_z(lift_height, t_, TT_, Tdsp);
+        if (var_theta * last_theta >= 0) {
+          Lt = wFootTheta(-var_theta, 0, t_, TT_, Tc_);
+          Rt = wFootTheta(-last_theta, 1, t_, TT_, Tc_);
+        }
+        else {
+          Lt = 0;
+          Rt = 0;
+        }
+      }
+      break;
+  }
+  if(now_step <= 2)
+  {
+    compensation_y_hip = (compensation_swing_hip + 0) * sin(M_PI * t_ / TT_) * M_PI / 180;
+    compensation_y_ankle = (compensation_swing_ankle + 0) * sin(M_PI * t_ / TT_) * M_PI / 180;
+  }
+  else
+  {
+    if (now_step % 2 == 0)
+    {
+      compensation_y_hip = (compensation_swing_hip + 0) * sin(M_PI * t_ / TT_) * M_PI / 180;
+      compensation_y_ankle = (compensation_swing_ankle - 1) * sin(M_PI * t_ / TT_) * M_PI / 180;
+    }
+    else if (now_step % 2 == 1)
+    {
+      compensation_y_hip = (compensation_swing_hip + 0) * sin(M_PI * t_ / TT_) * M_PI / 180;
+      compensation_y_ankle = (compensation_swing_ankle + 1) * sin(M_PI * t_ / TT_) * M_PI / 180;
+    }
+  }
+  // Cpy = Cpy + 0.1 * (Cpy - com_y);
+  // Cpx = Cpx - 0.1 * (Cpx - com_x);
+
+  
+  step_lxw = Lx - Cpx;
+  step_rxw = Rx - Cpx;
+
+  step_lyw = Ly - Cpy;
+  step_ryw = Ry - Cpy;
+
+  step_lx = step_lxw * cos(-var_theta) - step_lyw * sin(-var_theta);
+  step_ly = step_lxw * sin(-var_theta) + step_lyw * cos(-var_theta);
+  step_lz = Cpz - Lz;
+
+  step_rx = step_rxw * cos(-var_theta) - step_ryw * sin(-var_theta);
+  step_ry = step_rxw * sin(-var_theta) + step_ryw * cos(-var_theta);
+  step_rz = Cpz - Rz;
+
+  step_ltheta = -Lt;
+  step_rtheta = -Rt;
+
+  end_point_lx = step_lx;
+  end_point_ly = step_ly - width_size;
+  end_point_lz = step_lz - (COM_HEIGHT - STAND_HEIGHT);
+  
+  end_point_rx = step_rx;
+  end_point_ry = step_ry + width_size;
+  end_point_rz = step_rz - (COM_HEIGHT - STAND_HEIGHT);
+
+  end_point_ltheta = step_ltheta;
+  end_point_rtheta = step_rtheta;   
+
+  if (ready_to_stop) {
+    step_ = now_step + 1;
+    ready_to_stop = false;
+  }
+  sample_point++;
+}
+
+
+float WalkingGait::Swingfoot_pos_XY(float start, float length, float t, float T, float T_DSP){
+  float point = 0;
+  float new_t = t-T*T_DSP/2;
+  float omega = 2 * M_PI / (T * (1 - T_DSP));
+  float t1 = T * T_DSP / 2;
+  float t2 = T * (1 - T_DSP / 2);
+  
+  if (t > 0 && t <= t1) {
+      point = start;
+  }
+  else if (t > t1 && t <= t2) {
+      point = 2 * length * (omega*new_t-sin(omega*new_t)) / (2*M_PI) + start;
+  }
+  else {
+      point = 2 * length + start;
+  }
+  return point;
+}
+
+float WalkingGait::Swingfoot_pos_z(float lift_height, float t_now, float T, float Tdsp){
+  float pos;
+  float t1 = T * Tdsp / 2;
+  float t2 = T * (1 - Tdsp / 2);
+  float nt = t_now - t1;
+  float w = 2 * M_PI / (T * (1 - Tdsp));
+            
+  if (t1 < t_now && t_now <= t2) {
+      pos = 0.5 * lift_height * (1 - cos(w * nt));
+  }
+  else {
+      pos = 0;
+  }
+  return pos;
+}
+
+float WalkingGait::Com_vel(float x0, float xt, float px, float t, float Tc){
+  return (xt - x0 * cosh(t / Tc) + px * (cosh(t / Tc) - 1)) / (Tc * sinh(t / Tc));
+}
+
+float WalkingGait::Com_pos(float x0, float vx0, float px, float t, float Tc){
+  return px + x0 * cosh(t / Tc) + Tc * vx0 * sinh(t / Tc) - px * cosh(t / Tc);
+}
+
+float WalkingGait::wFootTheta(float theta, float reverse, float t, float T, float T_DSP){
+  float output;
+  float new_T = T*(1-T_DSP);
+  float new_t = t-T*T_DSP/2;
+  float omega = 2 * M_PI / (T * (1 - T_DSP));
+  float t1 = T * T_DSP / 2;
+  float t2 = T * (1 - T_DSP / 2);
+
+  if (t > 0 && t <= t1) {
+      if (reverse) {
+        output = theta;
+      }
+      else {
+        output = 0;
+      }
+  }
+  else if (t > t1 && t <= t2) {
+      if (reverse) {
+        output = 0.5*theta*(1-cos(0.5*omega*(new_t-new_T)));
+      }
+      else {
+        output = 0.5*theta*(1-cos(0.5*omega*new_t));
+      }
+  }
+  else if (t > t2) {
+      if (reverse) {
+        output = 0;
+      }
+      else {
+        output = theta;
+      }
+  }
+  else {
+    output = 0;
+  }
+  return output;
+}
+
+void WalkingGait::IMU_calculate(float roll, float pitch){
+  BLA::Matrix<3> WpA;   // World to Ankle
+  BLA::Matrix<3> BpA;   // Body to Ankle
+  BLA::Matrix<3> Theta; // [roll, pitch, yaw]
+  BLA::Matrix<3> WpB;   // World to Body result vector
+
+  // 設定 WpA、BpA、Theta
+  if (now_step % 2 == 1) {
+    WpA = {Lx, Ly, 0};
+    BpA = {end_point_lx, end_point_ly, -COM_HEIGHT};
+    Theta = {roll, pitch, theta_};
+  } else {
+    WpA = {Rx, Ry, 0};
+    BpA = {end_point_rx, end_point_ry, -COM_HEIGHT};
+    Theta = {roll, pitch, theta_};
+  }
+
+  // 計算 ZYX 旋轉矩陣
+  float cx = cos(Theta(0));
+  float sx = sin(Theta(0));
+  float cy = cos(Theta(1));
+  float sy = sin(Theta(1));
+  float cz = cos(Theta(2));
+  float sz = sin(Theta(2));
+
+  BLA::Matrix<3,3> R = {
+    cz * cy,             cz * sy * sx - sz * cx,   cz * sy * cx + sz * sx,
+    sz * cy,             sz * sy * sx + cz * cx,   sz * sy * cx - cz * sx,
+    -sy,                 cy * sx,                  cy * cx
+  };
+
+  // 執行向量減法：WpB = WpA - R * BpA
+  WpB = WpA - R * BpA;
+  com_x = WpB(0);
+  com_y = WpB(1);
+  // 若你要回傳 WpB，請加輸出參數或成員變數儲存
+}
+
+
+
+
+WalkingGait walking = WalkingGait();
---
+++ 0726/dynamixel_setup.cpp
@@ -0,0 +1,345 @@
+#include "dynamixel_setup.h"
+#include <math.h>
+#include <Arduino.h>
+
+control::control(){
+
+}
+void control::init(dynamixel::PortHandler *ph, dynamixel::PacketHandler *pkh) {
+  portHandler = ph;
+  packetHandler = pkh;
+}
+
+void control::motor_address(){
+  // define porthandler 
+  uint8_t dxl_error = 0;
+  int dxl_comm_result = COMM_TX_FAIL;
+  // for X-series use differen id to separate two kind motor
+  for (int id : {DXL20_ID, DXL21_ID, DXL26_ID, DXL27_ID}) {
+    // setup indrectadress
+    packetHandler->write2ByteTxRx(portHandler, id, ADDR_PRO_X_INDIRECTADDRESS_FOR_TORQUE, ADDR_PRO_X_TORQUE_ENABLE);
+    packetHandler->write2ByteTxRx(portHandler, id, ADDR_PRO_X_INDIRECTADDRESS_FOR_WRITE + 0, ADDR_PRO_X_PROFILE_VELOCITY + 0);
+    packetHandler->write2ByteTxRx(portHandler, id, ADDR_PRO_X_INDIRECTADDRESS_FOR_WRITE + 2, ADDR_PRO_X_PROFILE_VELOCITY + 1);
+    packetHandler->write2ByteTxRx(portHandler, id, ADDR_PRO_X_INDIRECTADDRESS_FOR_WRITE + 8, ADDR_PRO_X_PROFILE_VELOCITY + 4);
+    packetHandler->write2ByteTxRx(portHandler, id, ADDR_PRO_X_INDIRECTADDRESS_FOR_WRITE + 10, ADDR_PRO_X_PROFILE_VELOCITY + 5);
+  }
+  // for pro_series
+  for (int id : {DXL16_ID, DXL17_ID, DXL18_ID, DXL19_ID, DXL22_ID, DXL23_ID, DXL24_ID, DXL25_ID}) {
+    // setup indrectadress
+    packetHandler->write2ByteTxRx(portHandler, id, ADDR_PRO_H_INDIRECTADDRESS_FOR_WRITE + 0, ADDR_PRO_H_PROFILE_VELOCITY + 0);
+    packetHandler->write2ByteTxRx(portHandler, id, ADDR_PRO_H_INDIRECTADDRESS_FOR_WRITE + 2, ADDR_PRO_H_PROFILE_VELOCITY + 1);
+    packetHandler->write2ByteTxRx(portHandler, id, ADDR_PRO_H_INDIRECTADDRESS_FOR_WRITE + 4, ADDR_PRO_H_PROFILE_VELOCITY + 2);
+    packetHandler->write2ByteTxRx(portHandler, id, ADDR_PRO_H_INDIRECTADDRESS_FOR_WRITE + 6, ADDR_PRO_H_PROFILE_VELOCITY + 3);
+    packetHandler->write2ByteTxRx(portHandler, id, ADDR_PRO_H_INDIRECTADDRESS_FOR_WRITE + 8, ADDR_PRO_H_GOAL_POSITION + 0);
+    packetHandler->write2ByteTxRx(portHandler, id, ADDR_PRO_H_INDIRECTADDRESS_FOR_WRITE + 10, ADDR_PRO_H_GOAL_POSITION + 1);
+    packetHandler->write2ByteTxRx(portHandler, id, ADDR_PRO_H_INDIRECTADDRESS_FOR_WRITE + 12, ADDR_PRO_H_GOAL_POSITION + 2);
+    packetHandler->write2ByteTxRx(portHandler, id, ADDR_PRO_H_INDIRECTADDRESS_FOR_WRITE + 14, ADDR_PRO_H_GOAL_POSITION + 3);
+    packetHandler->write2ByteTxRx(portHandler, id, ADDR_PRO_H_INDIRECTADDRESS_FOR_TORQUE, ADDR_PRO_H_TORQUE_ENABLE);
+  }
+}
+
+void control::motor_torque(){
+  // Initialize GroupSyncWrite instance to initial data adress
+  dynamixel::GroupSyncWrite groupSyncWrite(portHandler, packetHandler, ADDR_PRO_INDIRECTDATA_FOR_TORQUE, LEN_PRO_GOAL_AND_VELOCITY);
+  uint8_t dxl_error = 0;
+  uint8_t param[1] = {TORQUE_ENABLE};;
+  // for X-series, the adress is velocity-position
+  for (int id = 15;id < 27;id++) {
+    groupSyncWrite.addParam(id+1, param);
+  }
+  groupSyncWrite.txPacket();
+  groupSyncWrite.clearParam();
+}
+
+void control::motor_torque_disable(){
+  // Initialize GroupSyncWrite instance to initial data adress
+  dynamixel::GroupSyncWrite groupSyncWrite(portHandler, packetHandler, ADDR_PRO_INDIRECTDATA_FOR_TORQUE, LEN_PRO_GOAL_AND_VELOCITY);
+  uint8_t dxl_error = 0;
+  uint8_t param[1] = {TORQUE_DISABLE};;
+  // for X-series, the adress is velocity-position
+  for (int id = 0;id < 27;id++) {
+    if (!groupSyncWrite.addParam(id+1, param)){
+      //serial.print("Failed to add parameter for ID: ");
+      //serial.println(id);
+    } 
+  }
+  int dxl_comm_result = groupSyncWrite.txPacket();
+  if (dxl_comm_result != COMM_SUCCESS){
+    //serial.print("Failed to send Sync write packet: ");
+    //serial.println(packetHandler->getTxRxResult(dxl_comm_result));
+  }
+  groupSyncWrite.clearParam();
+}
+
+
+void control::motor_groupSyncWrite(uint8_t position_velocity[96]){
+  // Initialize GroupSyncWrite instance to initial data adress
+  dynamixel::GroupSyncWrite groupSyncWrite(portHandler, packetHandler, ADDR_PRO_INDIRECTDATA_FOR_WRITE, LEN_PRO_GOAL_AND_VELOCITY);
+  uint8_t dxl_error = 0;
+  int id = 16;
+  // for X-series, the adress is velocity-position
+  for (int i = 0;i < 12;i++) {
+    // add information for every motor
+    id = i + 16;
+    uint8_t param_goal_position_velocity[LEN_PRO_GOAL_AND_VELOCITY];
+    param_goal_position_velocity[0] = position_velocity[0+i*8];
+    param_goal_position_velocity[1] = position_velocity[1+i*8];
+    param_goal_position_velocity[2] = position_velocity[2+i*8];
+    param_goal_position_velocity[3] = position_velocity[3+i*8];
+    param_goal_position_velocity[4] = position_velocity[4+i*8];
+    param_goal_position_velocity[5] = position_velocity[5+i*8];
+    param_goal_position_velocity[6] = position_velocity[6+i*8];
+    param_goal_position_velocity[7] = position_velocity[7+i*8];
+    if (!groupSyncWrite.addParam(id, param_goal_position_velocity)){
+      //serial.print("Failed to add parameter for ID: ");
+      //serial.println(id);
+    } 
+  }
+  // Send the packet to all Dynamixels
+  int dxl_comm_result = groupSyncWrite.txPacket();
+  if (dxl_comm_result != COMM_SUCCESS){
+    //serial.print("Failed to send Sync write packet: ");
+    //serial.println(packetHandler->getTxRxResult(dxl_comm_result));
+    return;
+  }
+  groupSyncWrite.clearParam();
+}
+
+void control::tranfer_bit(uint8_t position_velocity[96]){//計算相差值
+  int now_position[12];
+  for (int i = 0;i < 12;i++) {
+    now_position[i] = 
+      (int)position_velocity[4+i*8] |
+      ((int)position_velocity[5+i*8] << 8) |
+      ((int)position_velocity[6+i*8] << 16) |
+      ((int)position_velocity[7+i*8] << 24);
+    relative_position[i] = now_position[i]-origin_position[i];
+  }
+}
+
+void control::walking_groupSyncWrite(int profile_velocity[12], int goal_position[12]){
+  // Initialize GroupSyncWrite instance to initial data adress
+  dynamixel::GroupSyncWrite groupSyncWrite(portHandler, packetHandler, ADDR_PRO_INDIRECTDATA_FOR_WRITE, LEN_PRO_GOAL_AND_VELOCITY);
+  uint8_t dxl_error = 0;
+  int id = 16;
+  // for X-series, the adress is velocity-position
+  for (int i = 0;i < 12;i++) {
+    // add information for every motor
+    id = i + 16;
+    uint8_t param_goal_position_velocity[LEN_PRO_GOAL_AND_VELOCITY];
+    param_goal_position_velocity[0] = DXL_LOBYTE(DXL_LOWORD(profile_velocity[i]));
+    param_goal_position_velocity[1] = DXL_HIBYTE(DXL_LOWORD(profile_velocity[i]));
+    param_goal_position_velocity[2] = DXL_LOBYTE(DXL_HIWORD(profile_velocity[i]));
+    param_goal_position_velocity[3] = DXL_HIBYTE(DXL_HIWORD(profile_velocity[i]));
+    param_goal_position_velocity[4] = DXL_LOBYTE(DXL_LOWORD(goal_position[i]));
+    param_goal_position_velocity[5] = DXL_HIBYTE(DXL_LOWORD(goal_position[i]));
+    param_goal_position_velocity[6] = DXL_LOBYTE(DXL_HIWORD(goal_position[i]));
+    param_goal_position_velocity[7] = DXL_HIBYTE(DXL_HIWORD(goal_position[i]));
+    if (!groupSyncWrite.addParam(id, param_goal_position_velocity)){
+      //serial.print("Failed to add parameter for ID: ");
+      //serial.println(id);
+    } 
+  }
+  // Send the packet to all Dynamixels
+  int dxl_comm_result = groupSyncWrite.txPacket();
+  if (dxl_comm_result != COMM_SUCCESS){
+    //serial.print("Failed to send Sync write packet: ");
+    //serial.println(packetHandler->getTxRxResult(dxl_comm_result));
+    return;
+  }
+  groupSyncWrite.clearParam();
+}
+
+void control::motor_groupSyncRead(){
+  bool dxl_addparam_result;
+  int id;
+  int dxl_comm_result = COMM_TX_FAIL;
+  int32_t dxl_present_position;
+  dynamixel::GroupSyncRead groupSyncRead(portHandler, packetHandler, ADDR_PRO_INDIRECTDATA_FOR_READ, LEN_PRO_PRESENT_POSITION);
+  for(id = 2;id < 3;id++) {
+    dxl_addparam_result = groupSyncRead.addParam(id);
+    if (dxl_addparam_result != true)
+    {
+      //serial.print("[ID:"); //serial.print(id); //serial.println("groupSyncRead addparam failed");
+    }
+  }
+  dxl_comm_result = groupSyncRead.txRxPacket();
+  if (dxl_comm_result != COMM_SUCCESS) //serial.print(packetHandler->getTxRxResult(dxl_comm_result));
+  for(id = 2;id < 3;id++) {
+    groupSyncRead.isAvailable(id, ADDR_PRO_INDIRECTDATA_FOR_READ, LEN_PRO_PRESENT_POSITION);
+    if (dxl_addparam_result != true)
+    {
+      //serial.print("[ID:"); //serial.print(id); //serial.println("groupSyncRead getdata failed");
+    }
+  }
+  for(id = 2;id < 3;id++) {
+    dxl_present_position = groupSyncRead.getData(id, ADDR_PRO_INDIRECTDATA_FOR_READ, LEN_PRO_PRESENT_POSITION);
+    //serial.print("[ID:"); //serial.print(id);
+    //serial.print("  PresPos:"); //serial.print(dxl_present_position);
+    //serial.println(" ");
+  }
+  groupSyncRead.clearParam();
+}
+
+void control::Inverse_kinematic(float end_point_x, float end_point_y, float end_point_z, float end_point_theta, int RL){
+  float L6 = -5.8;
+  float l = 25.0;
+  makeTransformMatrix(0, 0, end_point_theta, end_point_x, end_point_y, end_point_z);
+  // 拆解 T 變數
+  float nx = T[0][0];
+  float ny = T[1][0];
+  float nz = T[2][0];
+
+  float ox = T[0][1];
+  float oy = T[1][1];
+  float oz = T[2][1];
+
+  float ax = T[0][2];
+  float ay = T[1][2];
+  float az = T[2][2];
+
+  float px = T[0][3];
+  float py = T[1][3];
+  float pz = T[2][3];
+  float px2 = px + L6 * ax;
+  float py2 = py + L6 * ay;
+  float pz2 = pz + L6 * az;
+  float L = sqrt(px2 * px2 + py2 * py2 + pz2 * pz2);
+  theta[RL+3] = acos((L * L) / (2 * l * l) - 1);
+  float a = acos(L / (2 * l));
+
+  theta[RL+5] = atan2(py + l * ay, pz + l * az);
+  theta[RL+4] = -atan2(px + l * ax, sqrt((py + l * ay) * (py + l * ay) + (pz + l * az) * (pz + l * az))) - a;
+
+  float s6 = sin(theta[RL+5]);
+  float c6 = cos(theta[RL+5]);
+  float c45 = cos(theta[RL+3] + theta[RL+4]);
+  float s45 = sin(theta[RL+3] + theta[RL+4]);
+  float R_21 = ny * c45 + oy * s6 * s45 + ay * c6 * s45;
+  float R_22 = oy * c6 - ay * s6;
+  float R_13 = -nx * s45 + ox * s6 * c45 + ax * c6 * c45;
+  float R_23 = -ny * s45 + oy * s6 * c45 + ay * c6 * c45;
+  float R_33 = -nz * s45 + oz * s6 * c45 + az * c6 * c45;
+
+  theta[RL+0] = atan2(R_13, R_33);
+  float s1 = sin(theta[RL+0]);
+  float c1 = cos(theta[RL+0]);
+
+  theta[RL+1] = atan2(-R_23, R_13*s1+R_33*c1);
+  theta[RL+2] = atan2(R_21, R_22);
+}
+
+
+void control::makeTransformMatrix(float roll, float pitch, float yaw, float px, float py, float pz) {
+  // 三角函數
+  float cx = cos(roll);
+  float sx = sin(roll);
+  float cy = cos(pitch);
+  float sy = sin(pitch);
+  float cz = cos(yaw);
+  float sz = sin(yaw);
+
+  // ZYX rotation matrix
+  float R[3][3] = {
+    {cz * cy, cz * sy * sx - sz * cx, cz * sy * cx + sz * sx},
+    {sz * cy, sz * sy * sx + cz * cx, sz * sy * cx - cz * sx},
+    {-sy,     cy * sx,                cy * cx}
+  };
+
+  // 填入旋轉矩陣
+  for (int i = 0; i < 3; ++i) {
+    for (int j = 0; j < 3; ++j) {
+      T[i][j] = R[i][j];
+    }
+  }
+
+  // 位置向量 px, py, pz
+  T[0][3] = px;
+  T[1][3] = py;
+  T[2][3] = pz;
+
+  // 最下面那一列
+  T[3][0] = 0;
+  T[3][1] = 0;
+  T[3][2] = 0;
+  T[3][3] = 1;
+}
+
+void control::rad2motor(int RL){
+  float X_pi2output = 4096/(2*M_PI);
+  float H_pi2output = 303750/(2*M_PI);
+  float l = 5.7;
+  int d = 4;
+  float theta5_origin = theta[RL+4];
+  float theta6_origin = theta[RL+5];
+  float x = (d*sin(theta6_origin));
+  float theta56_new = asin(x/l);
+  if (RL == 0) {
+    theta[4] = theta56_new-theta5_origin;
+    theta[5] = theta56_new+theta5_origin;
+  }
+  else {
+    theta[RL+4] = -theta56_new-theta5_origin;
+    theta[RL+5] = -theta56_new+theta5_origin;
+  }
+  for (int i = 0;i < 4;i++){
+    motor_position[RL+i] = round(theta[RL+i]*H_pi2output);
+  }
+  for (int i = 4;i < 6;i++){
+    motor_position[RL+i] = round(theta[RL+i]*X_pi2output);
+  }
+  if (RL == 0) {
+    motor_position[0] = relative_position[0]-motor_position[0];
+    motor_position[1] = relative_position[1]+motor_position[1];
+    motor_position[3] = relative_position[3]-motor_position[3];
+    motor_position[4] = relative_position[4]+2048+motor_position[4];
+    motor_position[5] = relative_position[5]+2048+motor_position[5];
+  }
+  else {
+    motor_position[RL+0] = relative_position[RL+0]+motor_position[RL+0];
+    motor_position[RL+1] = relative_position[RL+1]+motor_position[RL+1];
+    motor_position[RL+3] = relative_position[RL+3]+motor_position[RL+3];
+    motor_position[RL+4] = relative_position[RL+4]+2048-motor_position[RL+4];
+    motor_position[RL+5] = relative_position[RL+5]+2048-motor_position[RL+5];
+  }
+  motor_position[RL+2] = relative_position[RL+2]-motor_position[RL+2];
+}
+
+void control::motor_speed(int motion_delay, int RL){
+  double different_theta[12];
+  for(int i = 0;i < 6;i++){
+    different_theta[RL+i] = abs(past_theta[RL+i] - theta[RL+i]);
+    past_theta[RL+i] = theta[RL+i];
+  }
+  for(int i = 0;i < 4;i++){
+    profile_velocity[RL+i] = 0;
+    profile_velocity[RL+1] = 10300;
+    profile_velocity[RL+2] = 3000;
+    profile_velocity[RL+3] = 0;
+    // profile_velocity[RL+i] = round((abs(different_theta[RL+i]) / (2*M_PI)) * (1000/(float)motion_delay) * 60 / 0.00329218 * 2);
+    // if (step_length > 10){
+    //   if (i < 4 & profile_velocity[RL+i] < 4000){
+    //     profile_velocity[RL+i] = 4000;
+    //  }
+    //   if (i == 4 & profile_velocity[RL+4] < 6000){
+    //     profile_velocity[RL+i] = 6000;
+    //   }
+    // }
+    // else{
+      // if (i < 4 & profile_velocity[RL+i] < 2000){
+      //   profile_velocity[RL+i] *= 1000;
+      // }
+    // }
+  }
+  for(int i = 4;i < 6;i++){
+    // if (profile_velocity[RL+i] < 2000)
+    // {
+    //   profile_velocity[RL+i] += 2000;
+    // }
+    // profile_velocity[RL+i] = profile_velocity[RL+i] / 80.5;
+    profile_velocity[RL+i] = round((abs(different_theta[RL+i]) / (2*M_PI)) * (1000/(float)motion_delay) * 60 / 0.229);
+  }
+}
+
+
+control setupControl = control();
